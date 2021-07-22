9b540d60956c5cedbb6606178911f4bd14fc1c98
José de Souza <josee.souzaaa@gmail.com>
2021-07-22 20:39:38 -0300

    Configurações de index

--- js/slide.js
+++ js/slide.js
@@ -53,9 +53,38 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this)
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2
+    return -(slide.offsetLeft - margin)
+  }
+
+  slidesConfig() {
+    this.slidesArray = [...this.slide.children].map(element => {
+      const position = this.slidePosition(element)
+      return { position, element }
+    })
+  }
+
+  slidesIndexNav(index) {
+    const last = this.slidesArray.length - 1
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: 0,
+      next: index === last ? undefined : index + 1
+    }
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slidesArray[index]
+    this.moveSlide(activeSlide.position)
+    this.slidesIndexNav(index)
+    this.dist.finalPosition = activeSlide.position
+  }
+
   init() {
     this.bindEvents()
     this.addSlideEvents()
+    this.slidesConfig()
     return this
   }
 }
